42ebb229506f7bcf6a55800a8448c8d85c4d80f1
GamerBachu <rajnisingh.mrs@gmail.com>
2026-03-11 20:41:11 +0530

    fix number

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,16 +3,30 @@ import type { InputHTMLAttributes } from "react";
 export type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
 const Input = ({ className = "", readOnly, ...props }: InputProps) => {
-    return (
-        <input
-            readOnly={readOnly}
-            className={`w-full px-3 py-1.5 text-sm border rounded bg-gray-50 dark:bg-gray-900 border-gray-200 dark:border-gray-700 ${readOnly
+
+    if (props?.type === "number")
+        return (
+            <input
+                readOnly={readOnly}
+                className={`w-full px-3 py-1.5 text-sm border rounded bg-gray-50 dark:bg-gray-900 border-gray-200 dark:border-gray-700 ${readOnly
                     ? "text-gray-500 cursor-not-allowed outline-none"
                     : "outline-none"
-                } ${className}`}
-            {...props}
-        />
-    );
+                    } ${className}`}
+                {...props}
+                step={"any"}
+            />
+        );
+    else
+        return (
+            <input
+                readOnly={readOnly}
+                className={`w-full px-3 py-1.5 text-sm border rounded bg-gray-50 dark:bg-gray-900 border-gray-200 dark:border-gray-700 ${readOnly
+                    ? "text-gray-500 cursor-not-allowed outline-none"
+                    : "outline-none"
+                    } ${className}`}
+                {...props}
+            />
+        );
 };
 
 export default Input;
